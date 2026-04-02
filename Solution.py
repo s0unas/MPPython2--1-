f9f949243ca5bff1903ab139437479a2f4233c75
s0unas <husseinalaily@gmail.com>
2026-04-02 13:28:29 -0400

    updated version of correct solution?

--- Solution.py
+++ Solution.py
@@ -20,57 +20,70 @@ class Solution:
         bandwidths = self.info["bandwidths"]
         isp_neighbors = self.graph[self.isp]
 
-        # 1. PRE-CALCULATE BFS from every ISP neighbor
-        neighbor_maps = {}
+        #instead of us running a single BFS from the ISP, we run a seperate bfs, precomputed
+        # from every single imm neighbor of the isp. we save it in a dic so now we have the paths already. we dont calc for each client
+        neighbormap = {}
         for n in isp_neighbors:
-            neighbor_maps[n] = bfs_path(self.graph, n, clients)
+            neighbormap[n] = bfs_path(self.graph, n, clients)
 
-        # 2. Urgency Sort (Prioritizing the 'impatient' high-payers)
+        # we need to sort the impatient clients to the top of the list, so if a <= 1.0 they are impatient and we give them
+        #a 1m multiplier so they are automaticly at the top of list. if they are patient so a > 1,
+        # we divive their payment by a.
         def get_priority(c):
             a = alphas.get(c, 1.0)
-            return (payments[c] * 1000000) if a <= 1.0 else (payments[c] / a)
+            if a <= 1.0:
+                return (payments[c] * 1000000)
+            else:
+                return (payments[c] / a)
         
         sorted_clients = sorted(clients, key=get_priority, reverse=True)
 
-        # 3. Smart Load Balancing
-        # reservation[node][time] = load
+        
+        #reservation[node (router id)][time] = load (time is the tick 1,2,3....)
+        # we create a timebased grip to see when a router is free. a router might be full at tick 1 but at tick 2 its empty
+        # by tracking usage per sec we know if Client A is using Node 9 at Time 1, Client B should either wait 
+        #or find a different neighbor to avoid a collision.
+        # i used a nested dict to avoid checking everyime if a node exits or not.
         reservation = collections.defaultdict(lambda: collections.defaultdict(int))
         final_paths = {}
 
+        # we go through each client and check which will be the fastest path, we need to set a standard (infinity)
+        # any pth that will beat the bestarrival time that becomes the new best path.     
         for client_id in sorted_clients:
-            best_path = None
-            best_arrival_time = float('inf')
+            bestpath = None
+            bestarrivaltime = float('inf')
             
-            # 4. Check every neighbor's map to find the FASTEST arrival
+            #we check every neighbor's map to find the FASTEST arrival
             for n in isp_neighbors:
-                if client_id not in neighbor_maps[n] or not neighbor_maps[n][client_id]:
+                if client_id not in neighbormap[n] or not neighbormap[n][client_id]:
                     continue
                 
-                # Potential path: [ISP, Neighbor, ... Path to Client]
-                potential_path = [self.isp] + neighbor_maps[n][client_id]
+                potential_path = [self.isp] + neighbormap[n][client_id]
                 
                 # Calculate what the ACTUAL delay will be in the simulator
                 # based on current reservations
-                simulated_time = 0
+                #we have to implement a queue sim, - check reserv grid for current node at simtime
+                # - if the router is full of highprio packets, so > bandwiths[u] we have to wait for the next
+                # - when we find a router that is not full, we step forw, +1 to sim and we get the next node.
+                simtime = 0
                 for i in range(len(potential_path) - 1):
                     u = potential_path[i]
-                    # If this node is 'full' at this time, we wait (simulator logic)
-                    while reservation[u][simulated_time] >= bandwidths[u]:
-                        simulated_time += 1
-                    simulated_time += 1
+                    #If this node isfull at this time, we wait 
+                    while reservation[u][simtime] >= bandwidths[u]:
+                        simtime += 1
+                    simtime += 1
                 
-                # We want the path that gets us there the EARLIEST
-                if simulated_time < best_arrival_time:
-                    best_arrival_time = simulated_time
-                    best_path = potential_path
+                if simtime < bestarrivaltime:
+                    bestarrivaltime = simtime
+                    bestpath = potential_path
 
-            # 5. Lock in the best path and reserve the slots
-            if best_path:
-                final_paths[client_id] = best_path
-                # Reserve the slots so the next client knows we're in the way
+            # we need to then finally chose the best path for each client.
+            if bestpath:
+                final_paths[client_id] = bestpath
+                # thenreserve the slots so the next client knows we're in the way
                 t = 0
-                for i in range(len(best_path) - 1):
-                    u = best_path[i]
+                for i in range(len(bestpath) - 1):
+                    u = bestpath[i]
                     while reservation[u][t] >= bandwidths[u]:
                         t += 1
                     reservation[u][t] += 1
